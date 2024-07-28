eb25683382935a8f190cdd4e5ff18bd77db8c5d8
maximiliano  lezcano <maximilianolezcano@live.com>
2024-07-27 21:51:32 -0300

    modificacion para la segunda prentrega

--- script.js
+++ script.js
@@ -34,34 +34,58 @@ if (edad > 18) {
             break;
     }
 
-    // const opcion ("¿Los escorpiones pertenecen a la misma familia que…?:\n1 - los ratones\n2 - las lagartijas\n3 - las arañas")
-    // if (opcion === 1) {
-    //     alert("LA RESPUESTA ES...")
-    // } else if (opcion === 2) {
-    //     alert("LA RESPUESTA ES...")
-    // } else if (opcion === 3) {
-    //     alert("LA RESPUESTA ES...")
-    // } else {
-    //     alert("Opción incorrecta, FIN")
-    // } 
-
-    // switch (opcion) {
-    //     case 1:
-    //         alert("INCORRECTA, Has perdido!")
-    //         break;
-    //     case 2:
-    //         alert("INCORRECTA, Has perdido!")
-    //         break;
-    //     case 3:
-    //         alert("- CORRECTA -")
-    //         break;
-    //     default:
-    //         alert("Opción incorrecta, FIN")
-    //         break;
-    // }
-
 } else {
     alert("STOP - hasta aqui llego, ¡Game over!")
 }
+
+    alert("igual, te regalo y te cuento, algunas caracteristicas interesantes sobre el Colibri")
+    alert("Saben cuanto mide un Colibri?... solamente su pico mide 2cm y su cuerpo alrededor de 17cm aprox.")
+    function suma(num1, num2) {
+        let res = num1 + num2;
+        return res
+    }
+    let Resultado = suma(2, 17);
+    alert(Resultado + " cm, en total, es lo que mide un colibri promedio.")
+
+
+    alert("Mas datos sobre nuestro querido Colibri:")
+
+    const colibri = {
+        Significado: "Alegria-Sanacion-Adaptabilidad",
+        color: "iridiscente - es decir cambia, de colores metálicos dependiendo desde que punto me mires",
+        rapidez: "70 aleteos por segundo el movimiento de sus alas",
+    };
+    let Significado = colibri.Significado;
+    console.log(Significado);
+
+    let color = colibri.color;
+    console.log(color);
+
+    for (let key in colibri) {
+        console.log(key,colibri[key]);
+    }
+
+    // ---guia ej.---
+
+    // const numeros = [1, 2, 3, 4, 5];
+    // const resultado = numeros.filter(numero => {
+    //     if (numero %2 === 0) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
+
+    // console.log("resultado", resultado);
+
+    const tipos = [
+        {colibri:'rutilante', tamaño: 20},
+        {colibri: 'pardo', tamaño: 11},
+        {colibri: 'orejimorado', tamaño: 12},
+        {colibri: 'verdemar', tamaño: 40}
+    ];
+
+    const grandes = tipos.filter(p => p.tamaño >= 15);
+    console.log(grandes);
 
 alert("FIN")
